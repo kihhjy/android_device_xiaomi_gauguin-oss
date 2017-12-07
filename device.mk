aeb62d9c22cd6d4247f257fd9c2db9759a146a7d
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    gauguin: Build dummy android.hidl.base@1.0
    [TH779]: Convert to blueprint, build separate system_ext and vendor libs
             and remove libhidltransport dependency.
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c
    Signed-off-by: TH779 <i@779.moe>

--- device.mk
+++ device.mk
@@ -14,3 +14,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor
