213caad35e4a4214fb617e3908ba0ab18ac2aa7c
TH779 <i@779.moe>
2021-06-01 16:01:21 +0800

    gauguin: Initial skeleton tree
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I21b59a6c243d9466c94b8193138b6ab53aa6cbc2
    Signed-off-by: baolong24 <1667375308@qq.com>

---
+++ device.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
