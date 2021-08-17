AdarshGrewal/android_device_xiaomi_gauguin

gauguin: Import Xiaomi TouchFeature HAL definitions

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/gauguin/interfaces"

kihhjy/android_device_xiaomi_gauguin-oss

interfaces/update-makefiles.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.xiaomi:device/xiaomi/picasso/interfaces"